699af09dceb156d84b4fbc9a5f2a13b7bd56442c
Christian Blume <chr.blume@gmail.com>
2016-05-17 00:10:00 -0600

    some clean-up; more tests

--- src/cxxpool.h
+++ src/cxxpool.h
@@ -4,7 +4,6 @@
 #include <stdexcept>
 #include <queue>
 #include <utility>
-#include <list>
 #include <functional>
 #include <memory>
 #include <cstddef>
@@ -22,135 +21,158 @@ class thread_pool_error : public std::runtime_error {
 class thread_pool {
  public:
 
-  explicit thread_pool(int max_n_threads)
-  : threads_{}, done_{false}, max_n_threads_{max_n_threads}, tasks_{},
-    cond_var_{}, mutex_{}
-  {
-    if (max_n_threads_ <= 0)
-      max_n_threads_ = hardware_concurrency();
-  }
+  thread_pool();
 
-  thread_pool()
-  : thread_pool{0}
-  {}
+  explicit thread_pool(int n_threads);
 
   thread_pool(const thread_pool&) = delete;
   thread_pool& operator=(const thread_pool&) = delete;
   thread_pool(thread_pool&&) = delete;
   thread_pool& operator=(thread_pool&&) = delete;
 
-  ~thread_pool() {
-    {
-      std::lock_guard<std::mutex> lock{mutex_};
-      done_ = true;
-    }
-    cond_var_.notify_all();
-    for (auto& thread : threads_)
-      thread.join();
-  }
+  ~thread_pool();
 
-  int max_n_threads() const noexcept {
-    return max_n_threads_;
-  }
-
-  template<typename Functor, typename... Args>
-  auto push(int priority, Functor&& functor, Args&&... args)
-    -> std::future<typename std::result_of<Functor(Args...)>::type> {
-    if (priority < 0)
-      throw thread_pool_error{"priority cannot be smaller than zero"};
-    using result_type = typename std::result_of<Functor(Args...)>::type;
-    auto pack_task = std::make_shared<std::packaged_task<result_type()>>(
-      std::bind(std::forward<Functor>(functor), std::forward<Args>(args)...));
-    auto future = pack_task->get_future();
-    {
-        std::lock_guard<std::mutex> lock{mutex_};
-        tasks_.emplace([pack_task]{ (*pack_task)(); }, priority);
-        if (threads_.size() < static_cast<unsigned>(max_n_threads_) &&
-            tasks_.size() > threads_.size()) {
-          threads_.emplace_back(&thread_pool::run, this);
-        }
-    }
-    cond_var_.notify_one();
-    return future;
-  }
+  int n_threads() const noexcept;
 
   template<typename Functor, typename... Args>
   auto push(Functor&& functor, Args&&... args)
-    -> std::future<typename std::result_of<Functor(Args...)>::type> {
-    return push(0, std::forward<Functor>(functor), std::forward<Args>(args)...);
-  }
+    -> std::future<typename std::result_of<Functor(Args...)>::type>;
+
+  template<typename Functor, typename... Args>
+  auto push(int priority, Functor&& functor, Args&&... args)
+    -> std::future<typename std::result_of<Functor(Args...)>::type>;
 
  private:
 
-  void run() {
-    for (;;) {
-      priority_task task;
-      {
-        std::unique_lock<std::mutex> lock{mutex_};
-        cond_var_.wait(lock, [this]{ return done_ || !tasks_.empty(); });
-        if (done_ && tasks_.empty())
-            break;
-        task = tasks_.top();
-        tasks_.pop();
-      }
-      task.callback();
-    }
-  }
+  void run_tasks();
 
-  int hardware_concurrency() const noexcept {
-    const auto n_threads = std::thread::hardware_concurrency();
-    if (n_threads == 0)
-      throw thread_pool_error{
-      "invalid value from std::thread::hardware_concurrency()"};
-    return static_cast<int>(n_threads);
-  }
+  int hardware_concurrency() const noexcept;
 
   struct priority_task {
-
     static std::vector<std::uint64_t> task_counter_;
-
-    priority_task()
-    : callback{}, priority{}, order{}
-    {}
-
-    priority_task(std::function<void()> callback, int priority)
-    : callback{std::move(callback)}, priority{priority}, order{}
-    {
-      constexpr auto max = std::numeric_limits<
-          typename decltype(task_counter_)::value_type>::max();
-      if (task_counter_.empty() || task_counter_.back() == max)
-        task_counter_.push_back(0);
-      else
-        ++task_counter_.back();
-      order = task_counter_;
-    }
+    priority_task();
+    priority_task(std::function<void()> callback, int priority);
 
     std::function<void()> callback;
     int priority;
     decltype(task_counter_) order;
 
-    bool operator<(const priority_task& other) const {
-      if (priority == other.priority) {
-        if (order.size() == other.order.size()) {
-          return order.back() > other.order.back();
-        } else {
-          return order.size() > other.order.size();
-        }
-      } else {
-        return priority < other.priority;
-      }
-    }
+    bool operator<(const priority_task& other) const;
   };
 
-  std::list<std::thread> threads_;
   bool done_;
-  int max_n_threads_;
+  std::vector<std::thread> threads_;
   std::priority_queue<priority_task> tasks_;
   std::condition_variable cond_var_;
   std::mutex mutex_;
 };
 
 std::vector<std::uint64_t> thread_pool::priority_task::task_counter_{};
+
+thread_pool::thread_pool()
+: thread_pool{0}
+{}
+
+thread_pool::thread_pool(int n_threads)
+: done_{false}, threads_{}, tasks_{},
+  cond_var_{}, mutex_{}
+{
+  if (n_threads <= 0)
+    n_threads = hardware_concurrency();
+  threads_ = std::vector<std::thread>(n_threads);
+  for (auto& thread : threads_)
+    thread = std::thread{&thread_pool::run_tasks, this};
+}
+
+thread_pool::~thread_pool() {
+  {
+    std::lock_guard<std::mutex> lock{mutex_};
+    done_ = true;
+  }
+  cond_var_.notify_all();
+  for (auto& thread : threads_)
+    thread.join();
+}
+
+int thread_pool::n_threads() const noexcept {
+  return threads_.size();
+}
+
+template<typename Functor, typename... Args>
+auto thread_pool::push(Functor&& functor, Args&&... args)
+  -> std::future<typename std::result_of<Functor(Args...)>::type> {
+  return push(0, std::forward<Functor>(functor), std::forward<Args>(args)...);
+}
+
+template<typename Functor, typename... Args>
+auto thread_pool::push(int priority, Functor&& functor, Args&&... args)
+  -> std::future<typename std::result_of<Functor(Args...)>::type> {
+  if (priority < 0)
+    throw thread_pool_error{"priority smaller than zero: " +
+                            std::to_string(priority)};
+  using result_type = typename std::result_of<Functor(Args...)>::type;
+  auto pack_task = std::make_shared<std::packaged_task<result_type()>>(
+    std::bind(std::forward<Functor>(functor), std::forward<Args>(args)...));
+  auto future = pack_task->get_future();
+  {
+      std::lock_guard<std::mutex> lock{mutex_};
+      tasks_.emplace([pack_task]{ (*pack_task)(); }, priority);
+  }
+  cond_var_.notify_one();
+  return future;
+}
+
+void thread_pool::run_tasks() {
+  for (;;) {
+    priority_task task;
+    {
+      std::unique_lock<std::mutex> lock{mutex_};
+      cond_var_.wait(lock, [this]{ return done_ || !tasks_.empty(); });
+      if (done_ && tasks_.empty())
+          break;
+      task = tasks_.top();
+      tasks_.pop();
+    }
+    task.callback();
+  }
+}
+
+int thread_pool::hardware_concurrency() const noexcept {
+  const auto n_threads = std::thread::hardware_concurrency();
+  if (n_threads == 0)
+    throw thread_pool_error{
+    "got zero from std::thread::hardware_concurrency()"};
+  return static_cast<int>(n_threads);
+}
+
+thread_pool::priority_task::priority_task()
+: callback{}, priority{}, order{}
+{}
+
+thread_pool::priority_task::priority_task(
+    std::function<void()> callback, int priority)
+: callback{std::move(callback)}, priority{priority}, order{}
+{
+  constexpr auto max = std::numeric_limits<
+      typename decltype(task_counter_)::value_type>::max();
+  if (task_counter_.empty() || task_counter_.back() == max)
+    task_counter_.push_back(0);
+  else
+    ++task_counter_.back();
+  order = task_counter_;
+}
+
+bool thread_pool::priority_task::operator<(const priority_task& other) const {
+  if (priority == other.priority) {
+    if (order.size() == other.order.size()) {
+      return order.back() > other.order.back();
+    } else {
+      return order.size() > other.order.size();
+    }
+  } else {
+    return priority < other.priority;
+  }
+}
 
 
 }  // namespace cxxpool
--- test/cxxpool.cpp
+++ test/cxxpool.cpp
@@ -6,13 +6,13 @@ COLLECTION(test_cxxpool) {
 
 TEST(test_default_construction) {
   const cxxpool::thread_pool pool;
-  ASSERT_GREATER(pool.max_n_threads(), 0);
+  ASSERT_GREATER(pool.n_threads(), 0);
 }
 
 TEST(test_construct_with_thread_number) {
   const int threads = 4;
   const cxxpool::thread_pool pool{threads};
-  ASSERT_EQUAL(threads, pool.max_n_threads());
+  ASSERT_EQUAL(threads, pool.n_threads());
 }
 
 TEST(test_add_simple_task_void) {
@@ -41,6 +41,14 @@ TEST(test_add_various_tasks_with_priorities) {
   ASSERT_EQUAL(2., future2.get());
   ASSERT_EQUAL(6., future3.get());
   ASSERT_EQUAL(true, future4.get());
+}
+
+TEST(test_add_task_with_exception) {
+  cxxpool::thread_pool pool;
+  auto future1 = pool.push([]{ throw std::bad_alloc{}; return 1; });
+  auto future2 = pool.push([](double value) { return value; }, 2.);
+  ASSERT_THROW(std::bad_alloc, [&future1] { future1.get(); });
+  ASSERT_EQUAL(2., future2.get());
 }
 
 }
