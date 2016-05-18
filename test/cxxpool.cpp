d327e8e900eda0924c1e47b025ac9d24fa836b05
Christian Blume <chr.blume@gmail.com>
2016-05-17 23:33:35 -0600

    add support to run tests on VMs

--- test/cxxpool.cpp
+++ test/cxxpool.cpp
@@ -5,8 +5,12 @@
 COLLECTION(test_cxxpool) {
 
 TEST(test_default_construction) {
-  const cxxpool::thread_pool pool;
-  ASSERT_GREATER(pool.n_threads(), 0);
+  try {
+    const cxxpool::thread_pool pool;
+    ASSERT_GREATER(pool.n_threads(), 0);
+  } catch (const cxxpool::thread_pool_error&) {
+    // may get here on, e.g., VMs
+  }
 }
 
 TEST(test_construct_with_thread_number) {
@@ -16,7 +20,7 @@ TEST(test_construct_with_thread_number) {
 }
 
 TEST(test_add_simple_task_void) {
-  cxxpool::thread_pool pool;
+  cxxpool::thread_pool pool{2};
   int a = 1;
   auto future = pool.push([&a]{ a = 2; });
   future.get();
@@ -24,7 +28,7 @@ TEST(test_add_simple_task_void) {
 }
 
 TEST(test_add_two_tasks) {
-  cxxpool::thread_pool pool;
+  cxxpool::thread_pool pool{4};
   auto future1 = pool.push([]{ return 1; });
   auto future2 = pool.push([](double value) { return value; }, 2.);
   ASSERT_EQUAL(1, future1.get());
@@ -44,7 +48,7 @@ TEST(test_add_various_tasks_with_priorities) {
 }
 
 TEST(test_add_task_with_exception) {
-  cxxpool::thread_pool pool;
+  cxxpool::thread_pool pool{4};
   auto future1 = pool.push([]() -> int { throw std::bad_alloc{}; return 1; });
   auto future2 = pool.push([](double value) { return value; }, 2.);
   ASSERT_THROW(std::bad_alloc, [&future1] { future1.get(); });
