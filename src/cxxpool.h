f9665a8c97f4b957d4a65526cca455c534337fd7
Christian Blume <chr.blume@gmail.com>
2016-09-24 16:33:57 +1200

    change version to be under dev

--- src/cxxpool.h
+++ src/cxxpool.h
@@ -1,6 +1,6 @@
 /**
  * A portable, header-only thread pool for C++
- * @version 1.2.0
+ * @version 2.0.0 (under development)
  * @author Christian Blume (chr.blume@gmail.com)
  * @copyright 2015-2016 by Christian Blume
  * cxxpool is released under the MIT license:
