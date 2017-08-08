fd247465404176667c3c0275ecc6d1d5476ca013
Christian Blume <chr.blume@gmail.com>
2017-08-08 21:08:51 +1200

    run tests many times

--- make_check.sh
+++ make_check.sh
@@ -2,23 +2,35 @@
 set -e
 thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+compiler=g++
+if [ "x$1" != "x" ];then
+    compiler=$1
+fi
+export CXX=$compiler
+
 modes='debug release'
 
 for mode in $modes;do
-    echo "+++ Checking "$mode" ..."
+    echo "+++ Checking $mode with $compiler ..."
 
     # Checking tests
     $thisdir/src/compile_test.sh $mode
-    echo "+++ Running "$thisdir/src/test.$mode" ..."
-    $thisdir/src/test.$mode
+    echo "+++ Running $thisdir/src/test.$mode ..."
+    count=0
+    while [ $count -lt 100 ]; do
+        $thisdir/src/test.$mode > /dev/null
+        let count+=1
+    done
+    echo "Tests OK"        
 
     # Checking examples
     examples=$(ls $thisdir/examples/*.cpp)
     for ex in $examples; do
         $thisdir/examples/compile_example.sh $ex $mode
-        echo "+++ Running "${ex%.cpp}.$mode" ..."
+        echo "+++ Running ${ex%.cpp}.$mode ..."
         ${ex%.cpp}.$mode
     done
+    echo "Examples OK"        
 done
 
 echo "+++ Done!"
